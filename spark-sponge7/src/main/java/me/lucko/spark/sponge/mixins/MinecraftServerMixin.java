5fc0b39d3a2eae5901f10f8c83ee694b9da8b48d
44trent3 <trenten102579@gmail.com>
2022-03-12 14:03:14 -0600

    Add mixins to spark-sponge7, replace options.release w/toolchains

---
+++ spark-sponge7/src/main/java/me/lucko/spark/sponge/mixins/MinecraftServerMixin.java
@@ -0,0 +1,41 @@
+/*
+ * This file is part of spark.
+ *
+ *  Copyright (c) lucko (Luck) <luck@lucko.me>
+ *  Copyright (c) contributors
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package me.lucko.spark.sponge.mixins;
+
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(net.minecraft.server.MinecraftServer.class)
+public abstract class MinecraftServerMixin {
+    @Shadow
+    @Final
+    private static Logger LOGGER;
+
+    @Inject(method = "run", at = @At("HEAD"))
+    private void onRunHead(CallbackInfo ci) {
+        LOGGER.info("BEEP"); // Yeah, very professional testing here.
+    }
+}
